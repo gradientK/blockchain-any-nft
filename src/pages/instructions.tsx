3237a73a721590f0de6dd5261829a5c353e5ee71
Grant Sky <grantsky01@live.com>
2025-11-13 22:32:09 -0800

    some of instructions

--- src/pages/instructions.tsx
+++ src/pages/instructions.tsx
@@ -4,7 +4,14 @@ import { Link } from 'react-router-dom';
 export default function InstructionsMain() {
   return (
     <div>
-      <p>instructions</p>
+      <h2>How to Mint an NFT Instructions</h2>
+      <h3>Things you need:</h3>
+      <p>Dropbox account</p>
+      <p>Metamask Wallet browser extension</p>
+      <p>Polygon crypto tokens</p>
+      <br />
+      <h3>Step 1 - Dropbox</h3>
+      <p>Upload your future NFT's image to Dropbox</p>
       <br />
       <Link to="/mint">Now go mint a NFT</Link>
     </div>
