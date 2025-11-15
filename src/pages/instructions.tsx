496476dd9a12eadf6761b8f7efdca5efbb871951
Grant Sky <grantsky01@live.com>
2025-11-14 19:48:12 -0800

    latest

--- src/pages/instructions.tsx
+++ src/pages/instructions.tsx
@@ -16,33 +16,33 @@ export default function InstructionsMain() {
       <div className="instruction-step">
         <h3>Step 1 - MetaMask</h3>
         <p>Login to your MetaMask wallet browser extension</p>
-        <img src={require("../images/instructions/1login.jpg")} alt="Login to MetaMask" />
+        <img src={require("../images/instructions/1login.jpg")} alt="Login to MetaMask" className="img-login" />
       </div>
 
       <div className="instruction-step">
         <h3>Step 2 - Dropbox</h3>
         <p>1. Upload your future NFT's image to Dropbox</p>
-        <img src={require("../images/instructions/2upload.jpg")} alt="Upload to Dropbox" />
+        <img src={require("../images/instructions/2upload.jpg")} alt="Upload to Dropbox" className="img-upload" />
         
         <p>2. Select your image, click "Share", and click "Copy link"</p>
-        <img src={require("../images/instructions/3share.jpg")} alt="Dropbox click Share" />
+        <img src={require("../images/instructions/3share.jpg")} alt="Dropbox click Share" className="img-share" />
         
         <p>3. Ensure your image is shareable to "Anyone with link"</p>
-        <img src={require("../images/instructions/4anyonewithlink.jpg")} alt="Dropbox is shareable" />
+        <img src={require("../images/instructions/4anyonewithlink.jpg")} alt="Dropbox is shareable" className="img-anyone-with-link" />
       </div>
 
       <div className="instruction-step">
         <h3>Step 3 - Mint Form</h3>
         <p>1. Paste your Dropbox link into the Dropbox Address field</p>
         <p>2. Fill out the rest: NFT Name, Description, and Price (enter '0' if not for sale)</p>
-        <img src={require("../images/instructions/5form.jpg")} alt="Fill out form" />
+        <img src={require("../images/instructions/5form.jpg")} alt="Fill out form" className="img-form" />
         <p>3. Click "Mint NFT"</p>
       </div>
 
       <div className="instruction-step">
         <h3>Step 4 - Approve Transaction</h3>
         <p>MetaMask will ask for your approval. Review the transaction and click "Confirm"</p>
-        <img src={require("../images/instructions/6approve.jpg")} alt="Click Confirm" />
+        <img src={require("../images/instructions/6approve.jpg")} alt="Click Confirm" className="img-approve" />
       </div>
 
       <Link to="/mint" className="cta-link">
